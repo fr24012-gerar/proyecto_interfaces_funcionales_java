db47db412f147fdaaf6a8d3a4e70f8132a58997b
GerarFR21 <gerhardoadhonayflores@gmail.com>
2025-08-19 12:26:22 -0600

    cualquier cosa

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import interfaces_funcionales.Sumar;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Sumar sumar = (num1, num2) -> num1 + num2;
+        Sumar sumar_incrementar = (num1, num2) -> num1 + num2 + 1;
 
+        System.out.println("usando sumar = " + sumar.aplicar(2, 2));
+        System.out.println("usando sumar_incrementar = " + sumar_incrementar.aplicar(2, 2));
     }
 }
---
+++ src/interfaces_funcionales/Sumar.java
@@ -0,0 +1,6 @@
+package interfaces_funcionales;
+
+public interface Sumar {
+
+    double aplicar(double a, double b);
+}
