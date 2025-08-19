30870fe0f8bcb11f2229ff10cfc35b2af7a46479
GerarFR21 <gerhardoadhonayflores@gmail.com>
2025-08-19 12:58:15 -0600

    algo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import interfaces_funcionales.Sumar;
+import interfaces_funcionales.Transformar_String;
 
 public class Main {
 
@@ -7,7 +8,14 @@ public class Main {
         Sumar sumar = (num1, num2) -> num1 + num2;
         Sumar sumar_incrementar = (num1, num2) -> num1 + num2 + 1;
 
+        //Interface Transformar_String
+        Transformar_String aMayuscula = texto -> texto.toUpperCase();
+        Transformar_String aMinuscula = texto -> texto.toLowerCase();
+
         System.out.println("usando sumar = " + sumar.aplicar(2, 2));
         System.out.println("usando sumar_incrementar = " + sumar_incrementar.aplicar(2, 2));
+
+        System.out.println("usando aMayuscula: " + aMayuscula.transformar("juan"));
+        System.out.println("usando aMinuscula: " + aMinuscula.transformar("STANLEEE"));
     }
 }
